db960ab587f78a666c2eb8e433d7b780b3279771
kushalvk <kushalvaghela2003@gmail.com>
2025-09-23 00:37:00 +0530

    PPT

--- React CRUD/Demo_One/src/App.jsx
+++ React CRUD/Demo_One/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './Components/Home';
 import About from './Components/About';
 import Student from './Components/Student';
@@ -10,14 +10,14 @@ function App() {
   
   return (
     <>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/student" element={<Student />} />
           <Route path="/view" element={<View />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   )
 }
